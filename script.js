3938826284fae4f6bd1f1dd15e221bde1631a619
GangaAravind <102428325+GangaAravind@users.noreply.github.com>
2023-03-05 21:34:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function changecontent(){
+    let x=document.getElementById("know-more");
+    if(x.style.display=="block")
+    {
+        x.style.display="none";
+    }
+    else{
+        x.style.display="block";
+    }
+
+    
+}
+function change(){
+    let y=document.getElementById("know");
+    if(y.style.display=="block")
+    {
+        y.style.display="none";
+    }
+    else{
+        y.style.display="block";
+    }
+
+    
+}
+function content(){
+    let z=document.getElementById("more");
+    if(z.style.display=="block")
+    {
+        z.style.display="none";
+    }
+    else{
+        z.style.display="block";
+    }
+
+    
+}
+
+
